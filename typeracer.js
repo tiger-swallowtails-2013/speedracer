c218f34f5cbb371108e51e14b8fcc7d502d968f8
dbcapprentice <dbcapprentice@gmail.com>
2013-09-23 12:32:03 -0700

    check typed letter and print feedback

--- typeracer.js
+++ typeracer.js
@@ -19,14 +19,24 @@
 
 // }
 
-
 var keys_pressed = []
 
 document.onkeypress = function(event) {
 
   keys_pressed.push(String.fromCharCode(event.keyCode));
 
-  document.getElementById("test").innerHTML = keys_pressed.join("");
-  console.log(keys_pressed)
+  // document.getElementById("test").innerHTML = keys_pressed.join("");
+  // console.log(keys_pressed)
+  charCompare()
 }
 
+function charCompare(){
+  var last_key = keys_pressed[keys_pressed.length-1]
+  console.log(last_key)
+  // console.log(keys_pressed)
+  if (last_key === "a") {
+    document.getElementById("test").innerHTML = "Correct! you typed: " + last_key
+  } else {
+    document.getElementById("test").innerHTML = "please type a lowercase \"a\"!"
+  }
+}
