6baeef2e85d8d48d5089e9ef0e1982704ee4245d
dbcapprentice <dbcapprentice@gmail.com>
2013-09-23 13:08:04 -0700

    check every character of a string as user types it and return feedback

--- typeracer.js
+++ typeracer.js
@@ -1,42 +1,23 @@
-// function getText()
-// {
-//   var string = [];
-//   document.getElementById("input").addEventListener("keydown", string);
-//   console.log(string);
-//   if string[0] != nil {
-//       document.getElementById("test").innerHTML="chagned"
-//     };
-// }
 
-// function loadArray(array){
-//   var tempArray << array[0]
-//   document.getElementById("test").innerHTML=tempArray[0]
-// }
-
-// function makeChange(){
-//   var string = [];
-//   document.getElementById("input").addEventListener("keydown", loadArray(string));
-
-// }
-
-var keys_pressed = []
+var keys_pressed = [];
+var test_string = "Type these words";
 
 document.onkeypress = function(event) {
 
   keys_pressed.push(String.fromCharCode(event.keyCode));
 
-  // document.getElementById("test").innerHTML = keys_pressed.join("");
-  // console.log(keys_pressed)
   charCompare()
-}
+};
 
 function charCompare(){
   var last_key = keys_pressed[keys_pressed.length-1]
-  console.log(last_key)
-  // console.log(keys_pressed)
-  if (last_key === "a") {
+  // console.log(last_key)
+  if (last_key === test_string.charAt(0)) {
     document.getElementById("test").innerHTML = "Correct! you typed: " + last_key
+    test_string = test_string.slice(1);
   } else {
-    document.getElementById("test").innerHTML = "please type a lowercase \"a\"!"
+    document.getElementById("test").innerHTML = "please type a lowercase " + test_string.charAt(0)
   }
-}
+    // console.log(test_string)
+
+};
