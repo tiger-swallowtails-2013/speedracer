73128943d4c40e0a73b782a4d065941bb194b4b9
dbcapprentice <dbcapprentice@gmail.com>
2013-09-23 12:07:44 -0700

    typing on page is captured and printed

--- typeracer.js
+++ typeracer.js
@@ -1,5 +1,32 @@
-<SCRIPT LANGUAGE="JavaScript">
+// function getText()
+// {
+//   var string = [];
+//   document.getElementById("input").addEventListener("keydown", string);
+//   console.log(string);
+//   if string[0] != nil {
+//       document.getElementById("test").innerHTML="chagned"
+//     };
+// }
+
+// function loadArray(array){
+//   var tempArray << array[0]
+//   document.getElementById("test").innerHTML=tempArray[0]
+// }
+
+// function makeChange(){
+//   var string = [];
+//   document.getElementById("input").addEventListener("keydown", loadArray(string));
+
+// }
 
 
+var keys_pressed = []
 
-</SCRIPT>
+document.onkeypress = function(event) {
+
+  keys_pressed.push(String.fromCharCode(event.keyCode));
+
+  document.getElementById("test").innerHTML = keys_pressed.join("");
+  console.log(keys_pressed)
+}
+
