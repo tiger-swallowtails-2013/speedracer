83619ff3a1e8b599f180b9c05ad5a83160d1dd4a
dbcapprentice <dbcapprentice@gmail.com>
2013-09-23 17:57:42 -0700

    Added first Jasmine test related to function typeFollow

--- spec/index_spec.js
+++ spec/index_spec.js
@@ -5,18 +5,15 @@
 // the timer ends
 // alert sounds when end of sentence text is reached
 // alert tells wpm
-alert('Hi I\'m here!');
 
-
-// describe("Type follow", function() {
-//   it ("return a letter", function() {
-//     expect(typeFollow()).toMatch(/./);
-//   });
-// });
 
 
 describe("Type follow", function() {
   it ("returns a success with letter 'm' given and expected", function() {
-    expect(typeFollow(77)).andReturn("Success! You typed 'm'!");
+    expect(typeFollow(109)).toBe("Success! You typed 'm'!");
+  });
+
+  it ("returns a failure with letter 'z' given but not expected", function() {
+    expect(typeFollow(122)).toBe("Fail! You did not type 'm'!");
   });
 });
