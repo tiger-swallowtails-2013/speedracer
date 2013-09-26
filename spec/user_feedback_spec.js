21e697f6d6ce3d982092e3c112b5539904af123f
artfoundry <dave@artfoundry.com>
2013-09-26 16:43:50 -0700

    stub out end game and restart game tests

--- spec/user_feedback_spec.js
+++ spec/user_feedback_spec.js
@@ -1,46 +1,64 @@
-describe ('word_highlight', function(){
-  var text, span1;
-  beforeEach(function(){
-    span1 = document.createElement('span');
-    span1.id = "word1";
-    span1.innerHTML = "type";
-    document.body.appendChild(span1);
-    span2 = document.createElement('span');
-    span2.id = "word2";
-    span2.innerHTML = "these";
-    document.body.appendChild(span2);
-  });
-  afterEach(function(){
-    document.body.removeChild(span1);
-    document.body.removeChild(span2);
-  });
+// describe ('word_highlight', function(){
+//   var text, span1;
+//   beforeEach(function(){
+//     span1 = document.createElement('span');
+//     span1.id = "word1";
+//     span1.innerHTML = "type";
+//     document.body.appendChild(span1);
+//     span2 = document.createElement('span');
+//     span2.id = "word2";
+//     span2.innerHTML = "these";
+//     document.body.appendChild(span2);
+//   });
+//   afterEach(function(){
+//     document.body.removeChild(span1);
+//     document.body.removeChild(span2);
+//   });
 
-  it ('highlight first word', function(){
-    changeColor(1);
-    expect(span1.style.color).toBe('green');
-  });
+//   it ('highlight first word', function(){
+//     changeColor(1);
+//     expect(span1.style.color).toBe('green');
+//   });
 
-  it ('highlight subsequent word after previous word is typed correctly', function(){
-    changeColor(2);
-    expect(span2.style.color).toBe('green');
-  });
+//   it ('highlight subsequent word after previous word is typed correctly', function(){
+//     changeColor(2);
+//     expect(span2.style.color).toBe('green');
+//   });
 
-  it ('return previous word to original color', function(){
-    changeColor(2);
-    expect(span1.style.color).toBe('black');
-  });
+//   it ('return previous word to original color', function(){
+//     changeColor(2);
+//     expect(span1.style.color).toBe('black');
+//   });
+// });
+
+// describe ('gameplay', function(){
+//   beforeEach(function())
+//   it ('gets the first word'){
+//     // currentSentence = "type this stuff"
+//     // document.createElement('span')
+//   }
+//   it ('shifts current_word into past_text'){}
+//   it ('shifts new current_word from future_text'){}
+//   it ('maintains continuity of original text'){}
+// });
+
+describe ('endgame', function(){
+  it ('does not have a current_word'){
+    stringHandler = new ShiftWords();
+    stringHandler.initalize("A Test String")
+    currentWord = document.createElement("span").id = "current";
+    document.appendChild(currentWord);
+    stringHandler.shift()
+     stringHandler.shift()
+      stringHandler.shift()
+    expect(currentWord.innerText).toEqual("")
+  };
+  it ('displays the users time');
 });
 
-describe ('placeText', function(){
-  beforeEach(function())
-
-  it ('gets the first word'){
-    currentSentence = "type this stuff"
-    document.createElement('span')
-    
-  }
-  it ('shifts current_word into past_text'){}
-  it ('shifts new current_word from future_text'){}
-  it ('maintains continuity of original text'){}
+describe ('restartgame', function(){
+  it ('clears the input field');
+  it ('clears the time display');
+  it ('clears the past_text span element');
+  it ('populates the future_text span element');
 });
-
